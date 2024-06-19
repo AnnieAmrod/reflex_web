322b10e284c39a56920392c2bb395d5afc795294
Miriam <mymypedro96@gmail.com>
2024-06-19 16:33:30 +0200

    Preparación de API

--- reflex_web/reflex_web.py
+++ reflex_web/reflex_web.py
@@ -9,6 +9,7 @@ import reflex as rx
 import reflex_web.styles.styles as styles
 from reflex_web.pages.index import index  # *NO QUITAR, SINO NO FUNCIONA
 from reflex_web.pages.projects import projects  # *NO QUITAR, SINO NO FUNCIONA
+from reflex_web.api.api import hello
 
 
 class State(rx.State):
@@ -21,3 +22,9 @@ app = rx.App(
     stylesheets=styles.STYLESHEETS,
     style=styles.BASE_STYLE,
 )
+
+
+
+
+
+app.api.add_api_route('/hello', hello)
