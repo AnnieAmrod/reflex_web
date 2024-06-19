0c93affffbc230baffc987b0107a9471b8da121f
Miriam <mymypedro96@gmail.com>
2024-06-19 16:06:40 +0200

    CORS y preparación del backend

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ source .venv/bin/activate
 pip install --upgrade pip
 rm -rf public
 reflex init
-reflex export --frontend-only
+API_URL=https://reflex-web.onrender.com reflex export --frontend-only
 unzip frontend.zip -d public
 rm -f frontend.zip
 deactivate
--- rxconfig.py
+++ rxconfig.py
@@ -2,4 +2,9 @@ import reflex as rx
 
 config = rx.Config(
     app_name="reflex_web",
+    api_url="https://reflex-web.onrender.com",
+    cors_allowed_origins=[
+        'http://localhost:3000'
+        'https://about-me.reflex.run/'
+    ]
 )
